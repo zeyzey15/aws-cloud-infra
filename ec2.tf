aa0f8cece8c6828d814d8c3caf0b54d5fde8f1c7
Zeyneb <zeynabamralievna@gmail.com>
2025-06-15 18:10:09 +0300

    ec2 instance profile

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,25 @@
 module "ec2_instance" {
+  iam_instance_profile = aws_iam_instance_profile.giuseppe_profile.name 
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y snapd
+              systemctl enable snapd
+              systemctl start snapd
+              snap install amazon-ssm-agent --classic
+              systemctl enable snap.amazon-ssm-agent.amazon-ssm-agent.service
+              systemctl start snap.amazon-ssm-agent.amazon-ssm-agent.service
+              EOF 
 
   source  = "terraform-aws-modules/ec2-instance/aws"
 
   name = "giuseppe_companyrome"
 
   instance_type          = "t2.micro"
-  key_name               = "zey"
   monitoring             = true
+  ami                    = "ami-01f23391a59163da9" 
+  
   vpc_security_group_ids = [aws_security_group.giuseppe_sg.id] 
   subnet_id              = data.aws_subnets.default.ids[1]
 
@@ -24,4 +37,36 @@ resource "aws_security_group" "giuseppe_sg" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+}
+
+# IAM Assume Role Policy for EC2
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+# IAM Role for EC2 to use SSM
+resource "aws_iam_role" "giuseppe_role" {
+  name               = "test_role"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+# Attach the AmazonSSMManagedInstanceCore policy to the role
+resource "aws_iam_role_policy_attachment" "giuseppe_policy" {
+  role       = aws_iam_role.giuseppe_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# Create an IAM Instance Profile for the role
+resource "aws_iam_instance_profile" "giuseppe_profile" {
+  name = "EC2InstanceProfileSSM"
+  role = aws_iam_role.giuseppe_role.name
 }
